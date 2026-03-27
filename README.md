e199b61780c325fbe68aef2eb36d8bacd5f57cf0
wensheng246-prog <wensheng246@gmail.com>
2026-03-27 12:10:07 +0800

    TodoList

---
+++ README.md
@@ -0,0 +1,28 @@
+# To-do List
+>Hi, this is my first project!
+A simple todo list built with **Python** and **C**
+
+## Tech Stack
+| Technology | Purpose |
+|----------------------|---------|
+| **Python** | main application logic |
+| **CustomTkinter** | GUI Framework |
+| **C** | data stroage backend |
+| **PyInstaller** | package to executable |
+
+## clone project
+https://github.com/wensheng246-prog/TodoList_CTkinter_framework
+
+## install dependencies
+pip install customtkinter  
+pip install pyinstaller
+
+## run the application
+>>Please change the file location in main.py (line 4)
+main.py
+
+## built application
+>>In cmd prompt cd to your "../todolist/src" file and run the command below
+#### pyinstaller --onefile --windowed --name "TodoList" --add-data "..\include\libtd.dll;." --hidden-import customtkinter --hidden-import ctypes main.py
+>>After run the first command run the command below
+#### pyinstaller TodoList.spec
---
+++ src/todolist.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdbool.h>
+#define RED "\033[31m"
+#define WHITE "\033[37m"
+
+struct TOD{
+    char todo[100];
+    bool done;
+};
+
+int sztodo = sizeof(struct TOD);
+
+struct TOD todo;
+char todo_a[260];
+
+char* get_path(){
+    GetModuleFileName(GetModuleHandle(NULL), todo_a, 260);
+    char* last = NULL;
+    for (char* p = todo_a; *p; p++) {
+        if (*p == '\\') *p = '/';
+    }
+    for(int i = 0;i<1;++i){
+        last = strrchr(todo_a,'/');
+        if(last){
+            *last = '\0';
+        }
+    }
+    strcat(todo_a,"/todo.dat");
+    return todo_a;
+}
+
+bool tips(){
+    int result = MessageBoxW(NULL,L"Do you want to reset?",L"Reset Notice",MB_YESNO|MB_ICONQUESTION|MB_DEFBUTTON2);
+    switch (result){
+        case IDYES:
+            return true;
+        case IDNO:
+            return false;
+    }
+    return false;
+}
+
+void create(){
+    fopen(todo_a,"wb");
+}
+
+bool databaseerr(){
+    int result = MessageBoxW(NULL,L"Databased doesn't exist. Do you want to create a databased?",L"Error Notice",MB_YESNO|MB_ICONERROR|MB_DEFBUTTON2);
+    switch (result){
+        case IDYES:
+            create();
+            break;
+        case IDNO:
+            break;
+    }
+    FILE * fp = fopen(todo_a,"r+b");
+    if(fp == NULL){
+        int result = MessageBoxW(NULL,L"Sorry database didn't created.",L"Error Notice",MB_YESNO|MB_ICONERROR|MB_DEFBUTTON2);
+        switch (result){
+            case IDYES:
+                return false;
+            case IDNO:
+                return false;
+        }
+    }
+    return true;
+}
+
+bool notice(){
+    FILE * fp = fopen(todo_a,"r+b");
+    if(fp == NULL){
+        if(!databaseerr()){ return false; }
+    }
+    printf("\n"RED"If you are testing this application, after this please delete the file show below\n"WHITE);
+    printf(todo_a);
+    return true;
+}
+
+char * read(int i){
+    strcpy(todo.todo,"\0");
+    FILE * fp = fopen(get_path(),"r+b");
+    if(fp == NULL){
+        return todo.todo;
+    }
+
+    char * tmp = 0;
+    fseek(fp,i*sztodo,SEEK_SET);
+    if(fread(&todo,sztodo,1,fp)){
+        return todo.todo;
+    }
+    return todo.todo;
+}
+
+
+bool readb(int i){
+    strcpy(todo.todo,"\0");
+    FILE * fp = fopen(get_path(),"r+b");
+    if(fp == NULL){
+        return todo.done;
+    }
+
+    char * tmp = 0;
+    fseek(fp,i*sztodo,SEEK_SET);
+    if(fread(&todo,sztodo,1,fp)){
+        return todo.done;
+    }
+    return todo.done;
+}
+
+void write(char * in,bool done){
+    FILE * fp = fopen(get_path(),"a+b");
+    if (fp==NULL){
+        return;
+    }
+    fseek(fp,0,SEEK_END);
+    fwrite(in,sizeof(char[100]),1,fp);
+    fwrite(&done,sizeof(bool),1,fp);
+    fclose(fp);
+}
+
+// D:\todo_list\databased\todo.dat
+//gcc -m64 -shared -o D:\\todo_list\\include\\libtd.dll D:\todo_list\src\todolist.c
